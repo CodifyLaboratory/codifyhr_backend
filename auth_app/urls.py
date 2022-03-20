5786042a1e627b328db28b0b382b4c6636c2872d
unknown <maxlestor2@gmail.com>
2022-03-20 17:34:50 +0600

    add logout urls

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,5 +6,6 @@ from .views import *
 
 urlpatterns  = [
     path('login/', obtain_auth_token, name='user-obtain_auth_token'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('user/', PersonalRoomViewSet.as_view({'get': 'retrieve'})),
 ]

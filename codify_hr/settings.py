3fdb5f78c5a3f775fcd1d7cc93b0186ddd156c20
unknown <maxlestor2@gmail.com>
2022-03-31 13:03:46 +0600

    cors_allowed_headers

--- codify_hr/settings.py
+++ codify_hr/settings.py
@@ -25,6 +25,28 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = ['*']
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 
 REST_FRAMEWORK = {

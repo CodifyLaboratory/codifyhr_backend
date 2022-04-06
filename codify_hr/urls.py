7290a08f5f039f226eae2dbcad2351c4a184fa8d
unknown <maxlestor2@gmail.com>
2022-04-06 15:34:28 +0600

    rename endpoint login

--- codify_hr/urls.py
+++ codify_hr/urls.py
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('admin/', admin.site.urls),
     path('auth/', include("rest_framework.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
